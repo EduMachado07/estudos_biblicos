c2e82131ba59bf3c91022324434cee005424711f
eduardo <eduardo.silvamachado07@gmail.com>
2025-09-23 22:46:28 -0300

    feat(study): implementa ajustes nos estudos - novo slug e o atributo tags

--- server/src/repositories/IStudyRepository.ts
+++ server/src/repositories/IStudyRepository.ts
@@ -1,7 +1,8 @@
 import { Study } from '../entities/Study'
+import { IUserRepository } from './IUserRepository';
 
 export interface IStudyRepository {
-    create(data: Study): Promise<Study>;
+    create(data: Study, userRepository: IUserRepository): Promise<Study>;
     findStudies(offset: number, limit: number): Promise<{ studies: Study[]; length: number }>;
     findById(id: string): Promise<Study | null>;
     updateById(id:string, data: Partial<Study>): Promise<Study>;
--- server/src/repositories/implementations/MockStudyRepository.ts
+++ server/src/repositories/implementations/MockStudyRepository.ts
@@ -1,11 +1,37 @@
 import { Study } from "../../entities/Study";
 import { IStudyRepository } from "../IStudyRepository";
 import { BadRequest } from "../IErrorRepository";
+import { MockUserRepository } from "./MockUserRepository";
+import { IUserRepository } from "../IUserRepository";
 
 export let MockStudies: Study[] = [];
 
 export class MockStudyRepository implements IStudyRepository {
-  async create(data: Study): Promise<Study> {
+  async create(data: Study, userRepository: IUserRepository): Promise<Study> {
+    const authorName = await userRepository.FindUserById(data.author);
+    const authorSlug = authorName.name
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+
+    const titleSlug = data.title
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+
+    let slug = `${authorSlug}/${titleSlug}`;
+    let version = 2;
+
+    // loop até achar slug único
+    while (MockStudies.find((study) => study.slug === slug)) {
+      slug = `${authorSlug}/${titleSlug}/v${version}`;
+      version++;
+    }
+
+    data.slug = slug
+
     MockStudies.push(data);
 
     return data;
